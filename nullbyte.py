003d63e4dafa48455978d45906fac6483445ce86
Migoliatte <46922406+migoliatte@users.noreply.github.com>
2021-05-14 11:01:42 +0000

    Update nullbyte.py

--- nullbyte.py
+++ nullbyte.py
@@ -1,3 +1,7 @@
+import subprocess
+import re # https://regex101.com/
+import os
+
 def CheckNullBytes(shellcode):
     NB = False
     for i in range(0, len(shellcode), 2):
@@ -6,3 +10,27 @@ def CheckNullBytes(shellcode):
             print("NullBytes here : " + shellcode[i:i+2])
     if NB == False:
         print("No NullBytes.")
+    return("test")
+        
+
+
+name="calcul"
+os.system("nasm -f elf "+name+".asm  && ld "+name+".o -m elf_i386 -o "+name)
+p = subprocess.run('objdump -d '+name,shell=True,stdout=subprocess.PIPE)
+p = p.stdout.decode("utf-8")
+
+p=p.replace("\t","")
+result=re.findall(":[0-9a-f ]{21}",p)
+resultfinal=[]
+for res in result:
+    resultfinal.append(res[1:].replace(" ",""))
+
+#CheckNullBytes("".join(resultfinal))
+
+#print(str(int(len("".join(resultfinal))/2)))
+resultfinal="".join(resultfinal)
+second=""
+for i in range(0, len(resultfinal), 2):
+    second+="\\x"+resultfinal[i:i+2]
+
+print(second)
